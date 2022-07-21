6fddd5419b763a83e19337ba9df87e3c57eeb878
Gladson-Lalu <gladsonlalu2001@gmail.com>
2022-07-22 03:16:17 +0530

    Fixed name issue

--- src/components/lamp/Lamp.jsx
+++ src/components/lamp/Lamp.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import videoPlayback from "../../assets/videoplayback.webm";
 function Lamp() {
   return (
-    <video autoPlay loop id="myVideo" width="100%" height="100%">
+    <video autoPlay loop id="playbackVideo" width="100%" height="100%">
       <source src={videoPlayback} type="video/webm" />
     </video>
   );
